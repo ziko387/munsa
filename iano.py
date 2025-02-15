34f5935731376457efd26076f33c2c09ebed49e3
ziko <mwangiian973@gmail.com>
2025-02-15 19:10:56 +0300

    added new file

---
+++ iano.py
@@ -0,0 +1,7 @@
+myname=(input("enter your name:"))
+myage=int(input('Enter your age:'))
+
+if myage==25:
+    print(f"hellow {myname}, you are 25 years old")
+else:
+    print("invalid input")
---
+++ juma.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f" {num} is an even number.")
+else:
+    print(f" {num} is an odd number.")
+
+
+
+
+
+
+
