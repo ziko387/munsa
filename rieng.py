0e7a20a8c68f6e2f1ea4c32cb5c6408694298ec4
ziko <mwangiian973@gmail.com>
2025-02-16 22:30:01 +0300

    add a file

---
+++ rieng.py
@@ -0,0 +1,8 @@
+def is_palindrome(word):
+    word = word.lower()
+    return word == word[::-1]
+word = input("Enter a word: ")
+if is_palindrome(word):
+    print(f"{word} is a palindrome")
+else:
+    print(f"{word} is not a palindrome")
